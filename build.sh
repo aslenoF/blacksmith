861159b5c63be44efa87168aa174f51596bbb191
aslenoF <faridaje404@gmail.com>
2023-06-22 16:36:46 +0700

    setup

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u $source -b $branch -g default,-mips,-darwin,-notdefault
-git clone $local_manifest --depth 1 -b $branch .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync1 -j8
 
 # build rom
 source build/envsetup.sh
-lunch rom_codename-buildtype
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=
-export KBUILD_BUILD_HOST=
-export BUILD_USERNAME=
-export BUILD_HOSTNAME=
-make bacon -j8
+export KBUILD_BUILD_USER=aslenofarid
+export KBUILD_BUILD_HOST=android-build
+export BUILD_USERNAME=aslenofarid
+export BUILD_HOSTNAME=android-build
+mka bacon -j8
